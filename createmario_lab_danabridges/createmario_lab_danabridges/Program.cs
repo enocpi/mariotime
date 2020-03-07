737eb07498be1ffcb5b3ce12a878809f383950a7
enocpi <61128286+enocpi@users.noreply.github.com>
2020-03-06 19:37:26 -0600

    Add files via upload

---
+++ createmario_lab_danabridges/createmario_lab_danabridges/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace createmario_lab_danabridges
+{
+    class Program
+    { public struct mario
+        {
+            public enum size {
+                small,
+                large
+
+            }
+
+            public int numOfLives;
+            public string currentLevel;
+            public size sizeOfmario;
+                public bool canShootFireBalls;
+            public bool isJumping;
+            public int numOfcoins;
+
+
+        }
+        static void Main(string[] args)
+
+            
+
+        {
+            mario mario = new mario();
+            mario.numOfLives = 3;
+            mario.currentLevel = "world 1-1";
+            mario.sizeOfmario = mario.size.large;
+            mario.canShootFireBalls = true;
+            mario.isJumping = true;
+            mario.numOfcoins = 100;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
